69cd50a837fbeee3cbfd0e9e98109cfd2b590d4a
Akther Hamid <akther.hamid@brainstation-23.com>
2024-06-05 23:39:27 +0600

    Pom Framework message

--- src/test/java/ModuleObject/HomePageActionClass.java
+++ src/test/java/ModuleObject/HomePageActionClass.java
@@ -1,11 +1,10 @@
-package ActionClass;
-
+package ModuleObject;
 import ModuleObject.HomePageObject;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class HomePage {
+public class HomePageActionClass {
     static WebDriver driver=null;
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
@@ -13,6 +12,10 @@ public class HomePage {
 
         // project website visit
         driver.get("https://katalon-demo-cura.herokuapp.com/");
+
         HomePageObject.hamburgerMenu(driver).click();
+        //Thread.sleep(5000);
+        HomePageObject.Login(driver).click();
     }
 }
+
--- src/test/java/ModuleObject/HomePageObject.java
+++ src/test/java/ModuleObject/HomePageObject.java
@@ -1,2 +1,45 @@
-package ModuleObject;public class HomePageObject {
+package ModuleObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class HomePageObject {
+    private static WebElement element=null;
+    //WebDriver driver=null;
+
+    // hamburgerMenu button
+    public static WebElement hamburgerMenu(WebDriver driver)
+    {
+        element=driver.findElement(By.id("menu-toggle"));
+        return element;
+    }
+
+    // Home button
+    public static WebElement Home(WebDriver driver)
+    {
+        element=driver.findElement(By.linkText("Home"));
+        return element;
+    }
+
+    // Login button
+    public static WebElement Login(WebDriver driver)
+    {
+        element=driver.findElement(By.linkText("Login"));
+        return element;
+    }
+
+    // Make appointment button
+    public static WebElement MakeAppointment(WebDriver driver)
+    {
+        element=driver.findElement(By.id("btn-make-appointment"));
+        return element;
+    }
+
+    // to-top button
+    public static WebElement ToTop(WebDriver driver)
+    {
+        element=driver.findElement(By.id("to-top"));
+        return element;
+    }
 }
